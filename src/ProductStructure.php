e6caa6579075d1b7ace8afc654b9ac3a78968cb6
ocorvu <professorjoaodamata@gmail.com>
2022-02-16 18:57:59 -0300

    teste 100%

--- src/ProductStructure.php
+++ src/ProductStructure.php
@@ -1,6 +1,9 @@
 <?php
 namespace App;
 
+use RecursiveArrayIterator;
+use RecursiveIteratorIterator;
+
 class ProductStructure
 {
     const products = [
@@ -18,9 +21,31 @@ class ProductStructure
         "azul-P"
     ];
 
+    const colors = [
+        'preto',
+        'branco',
+        'vermelho',
+        'azul'
+    ];
+
     public function make(): array
     {
-       //todo your code.
-        return [];
+        $array = [];
+
+       foreach (self::products as $p) {
+           $product = explode('-', $p);
+
+           if (array_key_exists($product[0], $array)) {
+               array_push($array[$product[0]], $product[1]);
+           } else {
+               $array[$product[0]] = [$product[1]];
+           } 
+        }
+
+        foreach (self::colors as $c) {
+            $array[$c] = array_count_values($array[$c]);
+        }
+
+        return $array;
     }
 }
